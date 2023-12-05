d9ff2405a44c9b1b30541bfadbe5d1ad8582f7ed
93257037 <kristijan5541@gmail.com>
2023-12-05 22:32:33 +0100

    povezana baza

---
+++ HotelReservations/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservations
+{
+    public static class Config
+    {
+        public static string CONNECTION_STRING { get; } = "Data Source=LAPTOP-NBTNJTVJ\\SQLEXPRESS;Initial Catalog=HotelReservationsG2;Integrated Security=True";
+    }
+}
